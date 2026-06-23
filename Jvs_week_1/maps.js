1ec8cb6b516870766bd6c4f4e31b18311597a591
faizU804 <faizusardar900@gmail.com>
2026-06-23 06:34:51 -0700

    maps

---
+++ Jvs_week_1/maps.js
@@ -0,0 +1,17 @@
+// here we discuss about maps how we create and how we use as well 
+// Create a Map
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+// add map vaue 
+
+fruits.set("mangoose", 100)
+fruits.get("mangoose")
+
+console.log(fruits)
+
+
+// -----------------------------------------
